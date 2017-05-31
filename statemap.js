e4ddce9da66196b20d1203cee5d18a07bc53513c
Simon <simonbr73@gmail.com>
2017-05-30 22:25:14 -0500

    New functions to display percentage data by state/district

--- statemap.js
+++ statemap.js
@@ -1,6 +1,13 @@
-var width = 1400,
-    height = 800,
-    active = d3.select(null);
+var width = 950,
+    height = 550,
+    active = d3.select(null),
+    senateData,
+    houseData,
+    chamber;
+
+var color = d3.scale.linear()
+    .domain([0, 25, 50, 75, 100])
+    .range(colorbrewer.PuRd[5]);
 
 var zoom_c = false;
 
@@ -32,137 +39,123 @@ svg.call(zoom.event);
 
 d3.json("data/congressional_districts.json", function(congress) {
   d3.json("data/us.json", function(us) {
-    d3.csv("data/senate-members.csv", function(senate) {
-      d3.csv("data/house-members.csv", function(house) {
-        svg.append("defs").append("path")
-          .attr("id", "land")
-          .datum(topojson.feature(us, us.objects.land))
-          .attr("d", path);
-        svg.append("clipPath")
-          .attr("id", "clip-land")
-        .append("use")
-          .attr("xlink:href", "#land");
-
-        // Reorganize house data into a dictionary with district IDs as keys
-        house2 = {}
-        for (i=0; i<house.length; i++) {
-          house2[house[i].district_id] = house[i];
-        }
-
-        draw_districts(us, congress, house2);
-        //draw_states(us, congress, senate);
+    d3.json("data/senate-members.json", function(senate) {
+      d3.json("data/house-members.json", function(house) {
+        senateData = senate;
+        houseData = house;
+        //draw_districts(us, congress, houseData);
+        draw_states(us, congress, senateData);
       })
     })
+    svg.append("defs").append("path")
+      .attr("id", "land")
+      .datum(topojson.feature(us, us.objects.land))
+      .attr("d", path);
+    svg.append("clipPath")
+      .attr("id", "clip-land")
+    .append("use")
+      .attr("xlink:href", "#land");
   })
 });
 
-function draw_states(us, congress, senate){
+function draw_states(us, congress, senate, committee) {
+  chamber = 'senate';
   g.selectAll("path")
-      .data(topojson.feature(us, us.objects.states).features)
+      .data(topojson.feature(us, us.objects.states).features.filter(function(d) {
+        var state = d.id;
+        return state != 'DC' && state != 'PR' && state != 'VI';
+      }))
     .enter().append("path")
       .attr("d", path)
       .attr("class", "feature")
-      .attr('id', function(d, i) { return us.objects.states.geometries[i].id;})
-      .attr("fill", function(d, i) {
-        var state = us.objects.states.geometries[i].id;
-        var sen1, sen2;
-        if(state != 'DC' && state != 'PR' && state != 'VI'){
-          for (var j=0; j<101; j++){
-            var sen_home = senate[j].state;
-            if(state == sen_home && !sen1){
-              sen1 = senate[j];
-              for(var k=j+1; k<101; k++){
-                var sen_home2 = senate[k].state;
-                if(state == sen_home2 && !sen2){
-                  sen2 = senate[k];
-                }
-              }
-            }
-          }
+      .attr('id', function(d) { return d.id; })
+      .attr("fill", function(d) {
+        var state = d.id;
+        var sen1 = senate[state + 'a'];
+        var sen2 = senate[state + 'b'];
+        if (sen1.party == 'R' && sen2.party == 'R') { return '#e41a1c'; }
+        else if (sen1.party == 'D' && sen2.party == 'R') { return '#984ea3'; }
+        else if (sen1.party == 'R' && sen2.party == 'D') { return '#984ea3'; }
+        else if (sen1.party == 'D' && sen2.party == 'D') { return '#377eb8'; }
+        else if (sen1.party == 'I' && sen2.party == 'R' ||
+        sen1.party == 'R' && sen2.party == 'I') { return '#fff836'; }
+        else if (sen1.party == 'D' && sen2.party == 'I' ||
+        sen1.party == 'I' && sen2.party == 'D') { return '#00e7e7'; }
+        else if (sen1.party == 'I' && sen2.party == 'I') { return '#4daf4a'; }
+      })
 
-        if(sen1.party == 'R' && sen2.party == 'R') {return '#e41a1c';}
-        else if(sen1.party == 'D' && sen2.party == 'R') {return '#984ea3';}
-        else if(sen1.party == 'R' && sen2.party == 'D') {return '#984ea3';}
-        else if(sen1.party == 'D' && sen2.party == 'D') {return '#377eb8';}
-        else if(sen1.party == 'I' && sen2.party == 'R' ||
-        sen1.party == 'R' && sen2.party == 'I') {return '#fff836';}
-        else if(sen1.party == 'D' && sen2.party == 'I' ||
-        sen1.party == 'I' && sen2.party == 'D') {return '#00e7e7';}
-        else if(sen1.party == 'I' && sen2.party == 'I') {return '#4daf4a';}
-      }
-    })
+      .on("click", clicked)
 
-    .on("click", clicked)
+  /*
+      .on("mouseover", function(d){
+        var descrip = svg.append('g')
+          .attr('class', 'dummy_g');
 
-/*
-    .on("mouseover", function(d){
-      var descrip = svg.append('g')
-        .attr('class', 'dummy_g');
+        descrip.append('rect')
+            .attr('class', 'sendescrip')
+            .attr('width', width/5)
+            .attr('height', height)
+            .attr("transform", "translate(" + (width-width/5) +  ",0)");
 
-      descrip.append('rect')
-          .attr('class', 'sendescrip')
-          .attr('width', width/5)
-          .attr('height', height)
-          .attr("transform", "translate(" + (width-width/5) +  ",0)");
+        if (!zoom_c) d3.select("h1").text(statename(d));
 
-      if (!zoom_c) d3.select("h1").text(statename(d));
+        descrip.append("text")
+            .attr('class', 'sendescriph1')
+            .attr('x', width-width/5+10)
+            .attr("y", 20)
+            .attr("dy", ".35em")
+            .text(sen_header(d, senate, 0));
 
-      descrip.append("text")
-          .attr('class', 'sendescriph1')
-          .attr('x', width-width/5+10)
-          .attr("y", 20)
-          .attr("dy", ".35em")
-          .text(sen_header(d, senate, 0));
+        var sen_image_one = sen_header(d, senate, 0).replace(' ', '_');
+        descrip.append("svg:image")
+           .attr('x', width-width/5+20)
+           .attr('y', 50)
+           .attr('width', 100)
+           .attr('height', 152)
+           .attr("xlink:href","images/" + sen_image_one + ".jpg")
 
-      var sen_image_one = sen_header(d, senate, 0).replace(' ', '_');
+        descrip.append("text")
+            .attr('class', 'sendescriph2')
+            .attr('x', width-width/5+10)
+            .attr("y", height / 2)
+            .attr("dy", ".35em")
+            .text(sen_header(d, senate, 1));
+
+      var sen_image_two = sen_header(d, senate, 1).replace(' ', '_');
       descrip.append("svg:image")
          .attr('x', width-width/5+20)
-         .attr('y', 50)
+         .attr('y', (height/2 + 30))
          .attr('width', 100)
          .attr('height', 152)
-         .attr("xlink:href","images/" + sen_image_one + ".jpg")
+         .attr("xlink:href","images/" + sen_image_two + ".jpg")
 
-      descrip.append("text")
-          .attr('class', 'sendescriph2')
-          .attr('x', width-width/5+10)
-          .attr("y", height / 2)
-          .attr("dy", ".35em")
-          .text(sen_header(d, senate, 1));
+      })
+      .on("mouseout", function(d){
+          d3.select("h1").text('');
+          svg.selectAll('.dummy_g').remove();
+          svg.selectAll('.sendescrip').remove();
+          svg.selectAll('.sendescriph1').remove();
+          svg.selectAll('.sendescriph2').remove();
 
-    var sen_image_two = sen_header(d, senate, 1).replace(' ', '_');
-    descrip.append("svg:image")
-       .attr('x', width-width/5+20)
-       .attr('y', (height/2 + 30))
-       .attr('width', 100)
-       .attr('height', 152)
-       .attr("xlink:href","images/" + sen_image_two + ".jpg")
+      });
+      */
 
-    })
-    .on("mouseout", function(d){
-        d3.select("h1").text('');
-        svg.selectAll('.dummy_g').remove();
-        svg.selectAll('.sendescrip').remove();
-        svg.selectAll('.sendescriph1').remove();
-        svg.selectAll('.sendescriph2').remove();
-
-    });
-    */
-
-    g.append("path")
-        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-        .attr("class", "state-boundaries")
-        .attr("d", path);
+      g.append("path")
+          .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+          .attr("class", "state-boundaries")
+          .attr("d", path);
 }
 
-function draw_districts(us, congress, house, committee){
+function draw_districts(us, congress, house, committee) {
+  chamber = 'house';
   g.selectAll("path")
       .data(topojson.feature(congress, congress.objects.districts).features)
     .enter().append("path")
       .attr("class", "districts")
       .attr("clip-path", "url(#clip-land)")
       .attr("d", path)
-      .attr('fill', function(d, i){
-        var district = congress.objects.districts.geometries[i].id;
+      .attr('fill', function(d) {
+        var district = d.id;
         var distString = district.toString();
         if (district < 6000) {
           var rep = house[distString];
@@ -184,6 +177,34 @@ function draw_districts(us, congress, house, committee){
       .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
       .attr("class", "state-boundaries")
       .attr("d", path);
+}
+
+function showQuantitativeAttr(attr) {
+  if (chamber == 'senate') {
+    g.selectAll(".feature")
+        .attr("fill", function(d) {
+          var sen1 = senateData[d.id + 'a'];
+          var sen2 = senateData[d.id + 'b'];
+          var stateStat = (sen1[attr] + sen2[attr]) / 2.0;
+          return color(stateStat);
+        });
+  } else if (chamber == 'house') {
+    g.selectAll(".districts")
+        .attr("fill", function(d) {
+          var rep = houseData[d.id];
+          return color(rep[attr]);
+        });
+  }
+}
+
+function showMissedVotesPct() {
+  color.domain([0, 2, 4, 6, 8]);
+  showQuantitativeAttr('missed_votes_pct');
+}
+
+function showVotesWithPartyPct() {
+  color.domain([72, 79, 86, 93, 100]);
+  showQuantitativeAttr('votes_with_party_pct');
 }
 
 function clicked(d) {
@@ -220,6 +241,7 @@ function reset() {
 
 function zoomed() {
   g.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
+  g.selectAll("path").attr("d", path);
 }
 
 // If the drag behavior prevents the default click,
